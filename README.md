23b44ff7288d7a876a7d1890557830be010b8684
Ignasi López <ignasi.lopez.luna@gmail.com>
2024-10-27 21:21:04 +0100

    king - queen = man - woman

---
+++ README.md
@@ -0,0 +1,11 @@
+# Word2Vec
+
+This is a simple example of how to use Word2Vec in DL4J. The example uses the Google News Word2Vec model to find the closest words to a given word.
+
+## Requirements
+
+1. Java 23
+2. Download https://dl4jdata.blob.core.windows.net/resources/wordvectors/GoogleNews-vectors-negative300.bin.gz and store it as it is in resources folder.
+
+
+
---
+++ src/main/java/org/example/Load.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+@Slf4j
+public class Load
+{
+    @SneakyThrows
+    public static void main(String[] args )
+    {
+        log.info("Loading Google News Model...");
+        File gModel = new ClassPathResource("GoogleNews-vectors-negative300.bin.gz").getFile();
+        Word2Vec vec = WordVectorSerializer.readWord2VecModel(gModel);
+        log.info("Closest Words:");
+        Collection<String> kingList = vec.wordsNearest(Arrays.asList("king", "woman"), Collections.singletonList("queen"), 10);
+        log.info("Closest matches to 'king' on Google News: {}", kingList);
+    }
+}
